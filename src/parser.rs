3af8e2c66b9b7d1734553abbfe06a7ab7929ef91
Nikolai Shalakin <nikolai.shalakin@lifeisfeudal.com>
2024-10-16 19:40:31 +0200

    almost working precedence priorities with pratt parser

--- src/parser.rs
+++ src/parser.rs
@@ -7,7 +7,7 @@ use crate::parser::ast::{
     format_ast_short, ArithmeticExpressionNode, ArithmeticOperationNode, AstRootNode,
     CompareExpressionNode, CompareOperationNode, ExpressionNode, FunctionArgumentNode,
     FunctionCallNode, FunctionDataNode, FunctionsChainNode, LambdaNode, LiteralNode,
-    LogicExpressionNode, LogicOperationNode, RightExpressionPart, TernaryOperatorNode,
+    LogicExpressionNode, LogicOperationNode, TernaryOperatorNode,
 };
 use crate::parser::lexer::{LexerError, Token};
 use ast::Ast;
@@ -36,31 +36,60 @@ enum Assoc {
     Right,
 }
 
-// Operator precedence, from lowest to highest
-const PRECEDENCE_TABLE: &[(Token, usize, Assoc)] = &[
-    // (Token::Pipe, 1, Assoc::Left),         // functions_chain
-    (Token::Question, 2, Assoc::Right),    // ternary_operator
-    (Token::And, 3, Assoc::Left),          // logic_operation
-    (Token::Or, 3, Assoc::Left),           // logic_operation
-    (Token::Equal, 4, Assoc::Left),        // compare_operation
-    (Token::NotEqual, 4, Assoc::Left),     // compare_operation
-    (Token::Greater, 4, Assoc::Left),      // compare_operation
-    (Token::GreaterEqual, 4, Assoc::Left), // compare_operation
-    (Token::Less, 4, Assoc::Left),         // compare_operation
-    (Token::LessEqual, 4, Assoc::Left),    // compare_operation
-    (Token::Plus, 5, Assoc::Left),         // arithmetic_operation
-    (Token::Minus, 5, Assoc::Left),        // arithmetic_operation
-    (Token::Minus, 6, Assoc::Right),       // unary minus
-];
+#[derive(Copy, Clone, PartialEq, Eq, Ord, PartialOrd)]
+#[repr(u8)]
+enum PrecedenceLevel {
+    Lowest = 1,
 
-fn get_precedence_data(operation: &Token) -> Option<(usize, Assoc)> {
-    for (op, op_prec, assoc) in PRECEDENCE_TABLE {
-        if op == operation {
-            return Some((*op_prec, *assoc));
-        }
+    FunctionsChain = 10,
+    Ternary = 20,
+    Logic = 30,
+    Compare = 40,
+    Arithmetic = 50,
+    Unary = 60,
+
+    Strongest = 254,
+}
+
+impl PrecedenceLevel {
+    fn to_int(&self) -> u8 {
+        *self as u8
     }
 
-    None
+    fn slightly_increased(&self) -> u8 {
+        self.to_int() + 1
+    }
+
+    fn slightly_decreased(&self) -> u8 {
+        self.to_int() - 1
+    }
+}
+
+fn get_precedence(token: &Token) -> Option<PrecedenceLevel> {
+    use Token::*;
+    match token {
+        Plus => Some(PrecedenceLevel::Arithmetic),
+        Minus => Some(PrecedenceLevel::Arithmetic),
+        Equal => Some(PrecedenceLevel::Compare),
+        NotEqual => Some(PrecedenceLevel::Compare),
+        Less => Some(PrecedenceLevel::Compare),
+        LessEqual => Some(PrecedenceLevel::Compare),
+        Greater => Some(PrecedenceLevel::Compare),
+        GreaterEqual => Some(PrecedenceLevel::Compare),
+        And => Some(PrecedenceLevel::Logic),
+        Or => Some(PrecedenceLevel::Logic),
+        Pipe => Some(PrecedenceLevel::FunctionsChain),
+        Question => Some(PrecedenceLevel::Ternary),
+        _ => None,
+    }
+}
+
+fn get_precedence_level_assoc(operation: PrecedenceLevel) -> Assoc {
+    use PrecedenceLevel::*;
+    match operation {
+        Ternary | Unary => Assoc::Right,
+        _ => Assoc::Left,
+    }
 }
 
 impl<'input> Parser<'input> {
@@ -78,16 +107,16 @@ impl<'input> Parser<'input> {
 
     fn parse_program(&mut self) -> Result<AstRootNode, ParserError> {
         Ok(AstRootNode {
-            expression: self.parse_expression(None, 0)?,
+            expression: self.parse_expression(None, PrecedenceLevel::Lowest.to_int())?,
         })
     }
 
     fn parse_expression(
         &mut self,
         first_token: Option<Token>,
-        precedence: usize,
+        precedence: u8,
     ) -> Result<Box<ExpressionNode>, ParserError> {
-        let has_first_token = first_token.is_some();
+        //let has_first_token = first_token.is_some();
 
         let mut left = self.parse_primary_expression(first_token)?;
 
@@ -95,16 +124,21 @@ impl<'input> Parser<'input> {
         loop {
             let operation = self.lexer.next()?;
 
-            let precedence_data = get_precedence_data(&operation);
-            if precedence_data.is_none() {
+            let op_prec = get_precedence(&operation);
+            if op_prec.is_none() {
+                self.lexer.undo();
                 break;
             }
-            let (op_prec, assoc) = precedence_data.unwrap();
+
+            let op_prec = op_prec.unwrap();
 
             // Check if we should stop based on precedence
-            if op_prec < precedence {
+            if op_prec.to_int() < precedence {
+                self.lexer.undo();
                 break;
             }
+
+            let assoc = get_precedence_level_assoc(op_prec);
 
             // Special case for the ternary operator `? :`
             if operation == Token::Question {
@@ -117,10 +151,32 @@ impl<'input> Parser<'input> {
                 continue; // move to the next expression after handling ternary
             }
 
+            // Special case for pipe operator `|`
+            if operation == Token::Pipe {
+                let calls = self.parse_functions_chain()?;
+
+                let data = match *left {
+                    ExpressionNode::Literal(literal) => {
+                        Ok(Box::new(FunctionDataNode::Literal(literal)))
+                    }
+                    ExpressionNode::XValue => Ok(Box::new(FunctionDataNode::XValue)),
+                    c => Err(ParserError::Unexpected(format!(
+                        "Expected data for a functions chain, got {}",
+                        format_ast_short(&c)
+                    ))),
+                }?;
+
+                left = Box::new(ExpressionNode::FunctionsChain(FunctionsChainNode {
+                    data,
+                    function_calls: calls,
+                }));
+                continue; // move to the next expression after handling chain
+            }
+
             // Handle left or right associativity
             let next_prec = match assoc {
-                Assoc::Left => op_prec + 1,
-                Assoc::Right => op_prec,
+                Assoc::Left => op_prec.slightly_increased(),
+                Assoc::Right => op_prec.slightly_decreased(),
             };
 
             left = self.parse_infix(left, operation, next_prec)?;
@@ -133,7 +189,7 @@ impl<'input> Parser<'input> {
         &mut self,
         left: Box<ExpressionNode>,
         operation: Token,
-        precedence: usize,
+        precedence: u8,
     ) -> Result<Box<ExpressionNode>, ParserError> {
         use Token::*;
         match operation {
@@ -183,17 +239,6 @@ impl<'input> Parser<'input> {
                     },
                 )))
             }
-            // Question => Ok(Some(RightExpressionPart::TernaryOperator(
-            //     self.parse_ternary_operator()?,
-            // ))),
-            // Pipe => {
-            //     Ok(Box::new(ExpressionNode::FunctionsChain(
-            //         FunctionsChainNode {
-            //             data,
-            //             function_calls: calls,
-            //         },
-            //     )))
-            // },
             _ => Err(ParserError::Unexpected("Expected expression!".to_string())),
         }
     }
@@ -229,7 +274,7 @@ impl<'input> Parser<'input> {
             XValue => Ok(Box::new(ExpressionNode::XValue)),
             ParenthesisBegin => self.parse_parenthesis_expression(),
             Minus => Ok(Box::new(ExpressionNode::Negation(
-                self.parse_expression(None, 6)?,
+                self.parse_expression(None, PrecedenceLevel::Unary.to_int())?,
             ))),
             t => Err(ParserError::Unexpected(format!(
                 "Expected expression beginning, got {}",
@@ -239,7 +284,7 @@ impl<'input> Parser<'input> {
     }
 
     fn parse_parenthesis_expression(&mut self) -> Result<Box<ExpressionNode>, ParserError> {
-        let expr = self.parse_expression(None, 0)?;
+        let expr = self.parse_expression(None, PrecedenceLevel::Lowest.to_int())?;
 
         let token = self.lexer.next()?;
         if token == Token::ParenthesisEnd {
@@ -477,7 +522,7 @@ impl<'input> Parser<'input> {
     fn parse_ternary_operator(
         &mut self,
     ) -> Result<(Box<ExpressionNode>, Box<ExpressionNode>), ParserError> {
-        let true_expression = self.parse_expression(None, 0)?;
+        let true_expression = self.parse_expression(None, PrecedenceLevel::Ternary.to_int())?;
 
         let token = self.lexer.next()?;
         if token != Token::Colon {
@@ -487,7 +532,7 @@ impl<'input> Parser<'input> {
             )));
         }
 
-        let false_expression = self.parse_expression(None, 2)?;
+        let false_expression = self.parse_expression(None, PrecedenceLevel::Ternary.to_int())?;
 
         Ok((true_expression, false_expression))
     }
@@ -527,7 +572,10 @@ impl<'input> Parser<'input> {
                     }
                     ArrayBracketBegin | StringLiteral(_) | CharLiteral(_) | IntLiteral(_)
                     | BoolLiteral(_) | XValue | ParenthesisBegin | Minus => {
-                        FunctionArgumentNode::Expression(self.parse_expression(Some(token), 0)?)
+                        FunctionArgumentNode::Expression(self.parse_expression(
+                            Some(token),
+                            PrecedenceLevel::FunctionsChain.to_int(),
+                        )?)
                     }
                     Eof => break,
                     _ => {
@@ -558,7 +606,7 @@ impl<'input> Parser<'input> {
             self.lexer.undo();
             // HACK: Do not pass token! It'll stop full function chaining parsing
             // TODO: fix that hack inside `parse_expression -> parse_right_expression`
-            self.parse_expression(None, 0)?
+            self.parse_expression(None, PrecedenceLevel::Lowest.to_int())?
         };
 
         let token = self.lexer.next()?;
